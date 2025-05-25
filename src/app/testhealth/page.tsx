8decb64bae78b169b38a4605a5bd2104cc8b9ef1
Sami Fawcett <samifawcett.nyc@gmail.com>
2025-05-25 23:10:40 +0000

    client comp

--- src/app/testhealth/page.tsx
+++ src/app/testhealth/page.tsx
@@ -1,17 +1,23 @@
+"use client"; // Add this at the top
 import React from 'react';
 
+
 export default function TestHealth() {
-    return (
-        <div> 
-            <button onClick={() => {
-                fetch('/api/auth/health')
-                    .then(res => res.json())
-                    .then(data => 
-                        <p>
-                            {data.status} {data.message}
-                        </p>
-                    )
-            }}> Check health </button>
-        </div>
-    )
+  const handleClick = () => {
+    console.log("Button clicked");
+    fetch('/api/auth/health')
+      .then(res => res.json())
+      .then(data =>
+        <p>
+          {data.status} {data.message}
+        </p>
+      )
+  };
+
+  return (
+    <div>
+      <button onClick={handleClick}>Click me</button>
+    </div>
+  );
 }
+
